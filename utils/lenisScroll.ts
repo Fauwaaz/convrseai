f7c4d40c7621f56fa76801449287fa46c8e7c0db
Fauwaaz <fauwaazshaikh999@gmail.com>
2026-04-29 12:42:01 +0530

    fix lenis

--- utils/lenisScroll.ts
+++ utils/lenisScroll.ts
@@ -1,33 +1,33 @@
 
-import Lenis from "@studio-freight/lenis"
+// import Lenis from "@studio-freight/lenis"
 
-let lenis: Lenis | null = null
-let scroll = 0
+// let lenis: Lenis | null = null
+// let scroll = 0
 
-export function initLenis() {
-  if (lenis) return
+// export function initLenis() {
+//   if (lenis) return
 
-  lenis = new Lenis({
-    lerp: 0.02,          
-    smoothWheel: true,
-  })
+//   lenis = new Lenis({
+//     lerp: 0.02,          
+//     smoothWheel: true,
+//   })
 
-  lenis.on("scroll", (e: any) => {
-    scroll = e.scroll
-  })
+//   lenis.on("scroll", (e: any) => {
+//     scroll = e.scroll
+//   })
 
-  function raf(time: number) {
-    lenis!.raf(time)
-    requestAnimationFrame(raf)
-  }
+//   function raf(time: number) {
+//     lenis!.raf(time)
+//     requestAnimationFrame(raf)
+//   }
 
-  requestAnimationFrame(raf)
-}
+//   requestAnimationFrame(raf)
+// }
 
-export function getLenisScroll() {
-  return scroll
-}
+// export function getLenisScroll() {
+//   return scroll
+// }
 
-export function getLenisProgress() {
-  return scroll / window.innerHeight
-}
+// export function getLenisProgress() {
+//   return scroll / window.innerHeight
+// }
